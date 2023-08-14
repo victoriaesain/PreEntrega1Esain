6518cc3c9e04c4e1c82393f589c549b072377e6b
Victoria Esain <victoriaesain@hotmail.com>
2023-08-14 15:18:34 -0300

    componente Footer

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import NavBar from "./components/NavBar";
 import ItemListContainer from "./components/ItemListContainer";
+import Footer from "./components/Footer";
 
 function App() {
-
   return (
     <>
-      <NavBar/>
-      <ItemListContainer greeting={'Bienvenidos'} />
+      <NavBar />
+      <ItemListContainer greeting={"Bienvenidos"} />
+      <Footer />
     </>
   );
 }
 
 export default App;
-
-
-
